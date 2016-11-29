3cd355c7700c92a7304ec7b39306fd4671be2248
inyoungflork <ikwon01@risd.edu>
2016-11-29 13:14:14 -0500

    forglenn

---
+++ glenn/9/sketch.js
@@ -0,0 +1,59 @@
+var textBrush = "Hello";
+
+var textArray = ["Idiot Films", "pkitae333@naver.com", "Idiot Films", "Idiot Films","Idiot Films","Idiot Films"];
+
+
+//function preload() {
+//    for (var i = 0; i < emojiFileNames.length; i++){
+//        var filePath = "emoji/" + emojiFileNames[i];
+//        var emojiImage = loadImage(filePath);
+//        imageArray.push(emojiImage);
+//        }
+//    }
+
+//    var filePath = "emoji/" + random(emojiFileNames);
+//    emojiImage = loadImage(filePath);
+
+
+
+function setup() {
+    var theCanvas = createCanvas(windowWidth, windowHeight);
+    textSize(windowWidth / 12);
+    textAlign(CENTER);
+    
+    noFill();
+    stroke(random(200,255),0,0);
+    strokeWeight(3);
+    
+    frameRate(6);
+}
+
+function draw() {
+    
+    background(0,0,random(200,255),95);
+        
+    var randomFloat = random(textArray.length);
+    var randomInt = int(randomFloat);floor
+    
+    textBrush = textArray[randomInt];
+    text(textBrush, touchX, touchY);
+    
+    noFill();
+    stroke(random(200,255),0,0);
+    strokeWeight(3);
+    
+}
+
+
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+    
+}
+    
+    
+    
+    
+    
+    
